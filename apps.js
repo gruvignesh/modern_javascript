d3ff6d162eab50dd27925cbc34e31f88bff96a05
Guru Vignesh <guru.vignesh666@gmail.com>
2017-12-27 15:53:31 +0530

    console functionalities

--- apps.js
+++ apps.js
@@ -1,1 +1,27 @@
-alert("Hello World!")
+// alert("Hello World!")
+//Log to console
+/* multiline  comments*/
+
+//Console functionalities
+console.log("Hello world1");//Strings
+console.log(123);//numbers, chrome console numbers are blue
+console.log(true);//boolean,chrome console boolean datatype is in blue
+var greeting="Hello";//using variable in console
+console.log(greeting);
+console.log([1,2,3,4,5]);//arrays
+console.log({a:1, b:2});//objects
+console.table({a:1, b:2});//objects in table format
+
+console.error("This is some error");//error logging
+console.clear();//clear the console
+console.warn("This is a warning");//warning
+console.time('Hello');// time taken to execute a particular identifier, here, Hello
+  console.log("Hello world1");
+  console.log("Hello world1");
+  console.log("Hello world1");
+  console.log("Hello world1");
+  console.log("Hello world1");
+  console.log("Hello world1");
+  console.log("Hello world1");
+  console.log("Hello world1");
+console.timeEnd('Hello');
